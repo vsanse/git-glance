a331f6fef74db741784d046496e05c7caa64cce3
Vishal Sanserwal <vishal.sanserwal@go-mmt.com>
2021-02-23 16:11:48 +0530

    first commit

---
+++ src/main/main.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./main.scss";
+const setBg = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomColor;
+};
+export default function Main() {
+  const [userData, setUserData] = useState({});
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    axios.get(`https://api.github.com/users/vsanse`).then((resp) => {
+      setUserData(resp.data);
+    });
+    axios.get(`https://api.github.com/users/vsanse/repos`).then((resp) => {
+      setRepos(resp.data);
+    });
+  }, []);
+  return (
+    <main>
+      <section className="glass">
+        <div className="dashboard">
+          <div className="user">
+            <img src={userData?.avatar_url} alt="" />
+            <h3>{userData?.name}</h3>
+            <p>Frontend Engineer</p>
+            {userData?.html_url && (
+              <a href={userData?.html_url} className="follow_me">+ Follow</a>
+            )}
+          </div>
+          <div className="links"></div>
+        </div>
+        <div className="repos">
+          <div className="status">
+            <h1>Repositories</h1>
+            {/* <input type="text" /> */}
+          </div>
+          <div className="cards">
+            {repos.map((repo) => (
+              <div className="card" key={repo?.id}>
+                <div className="avatar_repo" style={{ background: setBg() }}>
+                  {repo.name.substring(0, 1)}
+                </div>
+                <div className="card-info">
+                  <h2>{repo?.name}</h2>
+                  <p className="repo_description">{repo?.description}</p>
+                  <a href={repo?.homepage || repo?.html_url} className="view_code" target="__blank">View Code</a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
